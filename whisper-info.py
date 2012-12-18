f7dbe47b18d915e42f723ec2c2e30a197d51e91f
Vo Minh Thu <noteed@gmail.com>
2012-12-19 00:29:24 +0100

    Turn whisper-info.py in a proper command-line tool.
    - Use argparse for command-line arguments parsing.
    - Display data similarly to `curved info`.

--- whisper-info.py
+++ whisper-info.py
@@ -1,18 +1,38 @@
 #! /usr/bin/env python2
-
-import sys
+import argparse
 import whisper
 
-if __name__ == '__main__':
-  print sys.argv
-  assert len(sys.argv) == 2
-
-  filename = sys.argv[1]
-  with open(filename, 'rb') as fh:
+def run(args):
+  with open(args.filename, 'rb') as fh:
     info = whisper.__readHeader(fh)
 
-    print "Metadata size: %s bytes." % whisper.metadataSize
-    print info
+    print '%s: period=%s, aggregation=%s, propagation=%s, archives=%s' % \
+      (args.filename, info['maxRetention'], info['aggregationMethod'],
+      info['xFilesFactor'], len(info['archives']))
 
-    xs = whisper.file_fetch(fh, 0, None)
-    print xs
+    for i, archive in enumerate(info['archives']):
+      print '  period=%s, points=%s, seconds=%s, size=%s, offset=%s' % \
+        (archive['retention'], archive['points'], archive['secondsPerPoint'],
+        archive['size'], archive['offset'])
+
+    if not args.header:
+      # Request all points until now. toInterval will be equal to now + step.
+      (fromInterval, toInterval, step), points = whisper.file_fetch(fh, 0, None)
+      print 'from=%s, to=%s, step=%s' % (fromInterval, toInterval, step)
+      if any(points):
+        for point in points:
+          print point
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Display a Whisper file's data."
+    )
+    parser.add_argument('filename', metavar='PATH',
+        help="Path to the Whisper file.")
+    parser.add_argument('--header', action='store_true',
+        help="Only display the header's data.")
+    parser.set_defaults(run=run)
+
+    args = parser.parse_args()
+    args.run(args)
