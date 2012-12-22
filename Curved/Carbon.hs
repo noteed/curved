4f9f5834f9c3633d9b929352fd554e31543c8781
Vo Minh Thu <noteed@gmail.com>
2012-12-22 18:24:14 +0100

    Split the metric name to create paths as Carbon does.

--- Curved/Carbon.hs
+++ Curved/Carbon.hs
@@ -3,11 +3,15 @@ module Curved.Carbon where
 
 import Control.Monad (forever, when)
 import Control.Concurrent (forkIO)
+import Data.List
+import System.FilePath ((</>), (<.>))
 import Network (accept, listenOn, withSocketsDo, PortID(..), Socket)
 import System.Directory (doesFileExist)
 import System.IO (hClose, hGetLine, hIsEOF, hPutStrLn, hSetBuffering, BufferMode(..), Handle)
 
 import Data.Whisper
+
+graphite_whisper_root = "/opt/graphite/storage/whisper"
 
 receivePoints port = do
   sock <- listenOn $ PortNumber port
@@ -27,12 +31,19 @@ process handle = do
         [metric, value_, timestamp_] ->
           case (validMetricName metric, reads value_, reads timestamp_) of
             (True, [(value :: Double, "")], [(timestamp :: Int, "")]) -> do
-              exist <- doesFileExist $ metric ++ ".wsp"
+              let path = foldl' (</>) graphite_whisper_root (split '.' metric) <.> "wsp"
+              exist <- doesFileExist path
               when (not exist) $
-                createWhisper (metric ++ ".wsp") [(60, 1440)] 0.5 Average
-              updateWhisperFile (metric ++ ".wsp") timestamp value
+                createWhisper path [(60, 1440)] 0.5 Average
+              updateWhisperFile path timestamp value
               process handle
             _ -> hClose handle
         _ -> hClose handle
 
-validMetricName = all (`elem` (['a'..'z'] ++ ['0'..'9'] ++ ".-"))
+validMetricName metric = all (`elem` (['a'..'z'] ++ ['0'..'9'] ++ ".-")) metric
+  && length metric > 0
+
+split c s =
+  case break (== c) s of
+    (before, "") -> [before]
+    (before, _ : after) -> before : split c after
