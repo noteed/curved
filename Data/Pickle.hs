573e7c7afd783b54183cbeaeb12e8c54462b4859
Vo Minh Thu <noteed@gmail.com>
2012-12-23 21:03:25 +0100

    Completed the pickling/unpickling code.
    Still not complete though. Now it supports some tuples, ints, and floats.

--- Data/Pickle.hs
+++ Data/Pickle.hs
@@ -7,20 +7,24 @@
 
 module Data.Pickle where
 
-import Control.Applicative ((<$>), (<*>), (*>))
-import Control.Monad (when)
+import Control.Applicative ((<$>), (*>))
 import Control.Monad.State
 import Control.Monad.Writer
 import qualified Data.ByteString as S
 import Data.Attoparsec hiding (take)
 import qualified Data.Attoparsec as A
-import Data.Attoparsec.Combinator
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as IM
 import Data.List (foldl')
 import Data.Map (Map)
 import qualified Data.Map as M
-import Data.Serialize.Put (runPut, putByteString, putWord8, Put)
+import Data.Serialize.Get (getWord64be, runGet)
+import Data.Serialize.Put (runPut, putByteString, putWord8, putWord64be, Put)
+import Data.Word (Word64)
+import Foreign.Marshal.Utils (with)
+import Foreign.Ptr (castPtr)
+import Foreign.Storable (peek)
+import System.IO.Unsafe (unsafePerformIO)
 
 -- Note \128 is the same as \STX\x80\02, i.e. protocol 2.
 
@@ -43,25 +47,35 @@ pickle value = runPut $ do
 -- instead of relying on attoparsec's "choice" combinator.
 
 opcodes :: Parser OpCode
-empty_dict :: Parser OpCode
+empty_dict, empty_list, tuple1, tuple2 :: Parser OpCode
 binput, mark :: Parser OpCode
+binint1, binfloat :: Parser OpCode
 short_binstring :: Parser OpCode
 setitem, setitems :: Parser OpCode
+append, appends :: Parser OpCode
 stop :: Parser OpCode
 
 opcodes = choice
-  [ empty_dict
+  [ empty_dict, empty_list, tuple1, tuple2
   , binput, mark
+  , binint1, binfloat
   , short_binstring
   , setitem, setitems
+  , append, appends
   , stop
   ]
 
 empty_dict = string "}" *> return EMPTY_DICT
 
+empty_list = string "]" *> return EMPTY_LIST
+
 binput = string "q" *> (BINPUT . fromIntegral <$> anyWord8)
 
 mark = string "(" *> return MARK
+
+binint1 = string "K" *> (BININT1 . fromIntegral <$> anyWord8)
+
+binfloat = string "G" *> (BINFLOAT <$> float8)
 
 short_binstring = do
   _ <- string "U"
@@ -69,25 +83,61 @@ short_binstring = do
   s <- A.take i
   return $ SHORT_BINSTRING s
 
+tuple1 = string "\133" *> return TUPLE1 -- same as \x85
+
+tuple2 = string "\134" *> return TUPLE2 -- same as \x86
+
 setitem = string "s" *> return SETITEM
 
 setitems = string "u" *> return SETITEMS
 
+append = string "a" *> return APPEND
+
+appends = string "e" *> return APPENDS
+
 stop = string "." *> return STOP
+
+float8 :: Parser Double
+float8 = do
+  w <- runGet getWord64be <$> A.take 8
+  case w of
+    Left err -> fail err
+    Right x -> return $ coerce x
+  where
+  coerce :: Word64 -> Double
+  coerce x = unsafePerformIO $ with x $ \p ->
+    peek (castPtr p) :: IO Double
 
 ----------------------------------------------------------------------
 -- Pickle opcodes serialization
 ----------------------------------------------------------------------
 
+serialize :: OpCode -> Put
 serialize opcode = case opcode of
   BINPUT i -> putByteString "q" >> putWord8 (fromIntegral i)
+  BININT1 i -> putByteString "K" >> putWord8 (fromIntegral i)
+  BINFLOAT d -> putByteString "G" >> putFloat8 d
   SHORT_BINSTRING s -> do
     putByteString "U"
     putWord8 . fromIntegral $ S.length s
     putByteString s
   EMPTY_DICT -> putByteString "}"
+  EMPTY_LIST -> putByteString "]"
+  TUPLE1 -> putByteString "\133"
+  TUPLE2 -> putByteString "\134"
   MARK -> putByteString "("
+  SETITEM -> putByteString "s"
   SETITEMS -> putByteString "u"
+  APPEND -> putByteString "a"
+  APPENDS -> putByteString "e"
+  x -> error $ "serialize: " ++ show x
+
+putFloat8 :: Double -> Put
+putFloat8 d = putWord64be (coerce d)
+    where
+    coerce :: Double -> Word64
+    coerce x = unsafePerformIO $ with x $ \p ->
+      peek (castPtr p) :: IO Word64
 
 ----------------------------------------------------------------------
 -- Pickle opcodes
@@ -95,11 +145,18 @@ serialize opcode = case opcode of
 
 data OpCode =
     EMPTY_DICT
+  | EMPTY_LIST
   | BINPUT Int
   | MARK
+  | BININT1 Int
+  | BINFLOAT Double
   | SHORT_BINSTRING S.ByteString
+  | TUPLE1
+  | TUPLE2
   | SETITEM
   | SETITEMS
+  | APPEND
+  | APPENDS
   | STOP
   deriving Show
 
@@ -110,6 +167,10 @@ data OpCode =
 -- Maybe I can call them Py? And Have IsString/Num instances?
 data Value =
     Dict (Map Value Value)
+  | List [Value]
+  | Tuple [Value]
+  | BinInt Int
+  | BinFloat Double
   | BinString S.ByteString
   | MarkObject -- Urk, not really a value.
   deriving (Eq, Ord, Show)
@@ -126,31 +187,52 @@ type Stack = [Value]
 type Memo = IntMap Value
 
 execute :: [OpCode] -> Stack -> Memo -> Either String Value
-execute [] [value] memo = Right value
+execute [] [value] _ = Right value
 execute (op:ops) stack memo = case executeOne op stack memo of
   Left err -> Left err
   Right (stack', memo') -> execute ops stack' memo'
 execute _ _ _ = Left "`execute` unimplemented"
 
+executePartial :: [OpCode] -> Stack -> Memo -> (Stack, Memo, [OpCode])
 executePartial [] stack memo = (stack, memo, [])
 executePartial (op:ops) stack memo = case executeOne op stack memo of
-  Left err -> (stack, memo, op:ops)
+  Left _ -> (stack, memo, op:ops)
   Right (stack', memo') -> executePartial ops stack' memo'
 
 executeOne :: OpCode -> Stack -> Memo -> Either String (Stack, Memo)
 executeOne EMPTY_DICT stack memo = return (Dict M.empty: stack, memo)
+executeOne EMPTY_LIST stack memo = return (List []: stack, memo)
 executeOne (BINPUT i) (s:stack) memo = return (s:stack, IM.insert i s memo)
+executeOne (BININT1 i) stack memo = return (BinInt i:stack, memo)
+executeOne (BINFLOAT d) stack memo = return (BinFloat d:stack, memo)
 executeOne (SHORT_BINSTRING s) stack memo = return (BinString s:stack, memo)
 executeOne MARK stack memo = return (MarkObject:stack, memo)
+executeOne TUPLE1 (a:stack) memo = return (Tuple [a]:stack, memo)
+executeOne TUPLE2 (b:a:stack) memo = return (Tuple [a, b]:stack, memo)
+executeOne SETITEM stack memo = executeSetitem stack memo
 executeOne SETITEMS stack memo = executeSetitems [] stack memo
+executeOne APPEND stack memo = executeAppend stack memo
+executeOne APPENDS stack memo = executeAppends [] stack memo
 executeOne STOP stack memo = Right (stack, memo)
-executeOne _ stack memo = Left "`executeOne` unimplemented"
+executeOne _ _ _ = Left "`executeOne` unimplemented"
 
+executeSetitem :: Monad m => Stack -> Memo -> m ([Value], Memo)
+executeSetitem (v:k:Dict d:stack) memo = return (Dict (M.insert k v d):stack, memo)
+
+executeSetitems :: Monad m => [(Value, Value)] -> Stack -> Memo -> m ([Value], Memo)
 executeSetitems l (MarkObject:Dict d:stack) memo = return (l `addToDict` Dict d:stack, memo)
 executeSetitems l (a:b:stack) memo = executeSetitems ((b, a) : l) stack memo
 
+executeAppend :: Monad m => Stack -> Memo -> m ([Value], Memo)
+executeAppend (x:List xs:stack) memo = return (List (xs ++ [x]):stack, memo)
+
+executeAppends :: Monad m => [Value] -> Stack -> Memo -> m ([Value], Memo)
+executeAppends l (MarkObject:List xs:stack) memo = return (List (xs ++ l):stack, memo)
+executeAppends l (x:stack) memo = executeAppends (x : l) stack memo
+
+addToDict :: [(Value, Value)] -> Value -> Value
 addToDict l (Dict d) = Dict $ foldl' add d l
-  where add d (a, b) = M.insert a b d
+  where add d' (k, v) = M.insert k v d'
 
 ----------------------------------------------------------------------
 -- Pickling (value to opcodes)
@@ -165,6 +247,10 @@ runPickler p = evalState (execWriterT (runP p)) M.empty
 pickle' :: Value -> Pickler ()
 pickle' value = case value of
   Dict d -> pickleDict d
+  List xs -> pickleList xs
+  Tuple xs -> pickleTuple xs
+  BinInt i -> pickleBinInt i
+  BinFloat d -> pickleBinFloat d
   BinString s -> pickleBinString s
   x -> error $ "TODO: pickle " ++ show x
 
@@ -179,13 +265,52 @@ binput' value = do
 pickleDict :: Map Value Value -> Pickler ()
 pickleDict d = do
   tell [EMPTY_DICT]
-  binput' (Dict d)
+  binput' (Dict M.empty)
 
   let kvs = M.toList d
-  when (not $ null kvs) $ do
-    tell [MARK]
-    mapM_ (\(k, v) -> pickle' k >> pickle' v) kvs
-    tell [SETITEMS]
+  case kvs of
+    [] -> return ()
+    [(k,v)] -> pickle' k >> pickle' v >> tell [SETITEM]
+    _ -> do
+      tell [MARK]
+      mapM_ (\(k, v) -> pickle' k >> pickle' v) kvs
+      tell [SETITEMS]
+
+pickleList :: [Value] -> Pickler ()
+pickleList xs = do
+  tell [EMPTY_LIST]
+  binput' (List [])
+
+  case xs of
+    [] -> return ()
+    [x] -> pickle' x >> tell [APPEND]
+    _ -> do
+      tell [MARK]
+      mapM_ pickle' xs
+      tell [APPENDS]
+
+pickleTuple :: [Value] -> Pickler ()
+pickleTuple [] = error "pickleTuple 0 TODO"
+pickleTuple [a] = do
+  pickle' a
+  tell [TUPLE1]
+  binput' (Tuple [a])
+pickleTuple [a, b] = do
+  pickle' a
+  pickle' b
+  tell [TUPLE2]
+  binput' (Tuple [a, b])
+pickleTuple _ = error "pickleTuple n TODO"
+
+-- TODO depending on the int range, it should not always be a BININT1
+pickleBinInt :: Int -> Pickler ()
+pickleBinInt i = do
+  tell [BININT1 i]
+
+-- TODO probably depends on the float range
+pickleBinFloat :: Double -> Pickler ()
+pickleBinFloat d = do
+  tell [BINFLOAT d]
 
 -- TODO depending on the string length, it should not always be a SHORT_BINSTRING
 pickleBinString :: S.ByteString -> Pickler ()
--- pickle-examples.sh
+++ pickle-examples.sh
@@ -9,4 +9,28 @@ runghc bin/pickle-read.hs example.pickle
 ./pickle-dump.py "{'type': 'cache-query', 'metric': 'some.metric.name'}" --output example.pickle
 runghc bin/pickle-read.hs example.pickle
 
+./pickle-dump.py "1" --output example.pickle
+runghc bin/pickle-read.hs example.pickle
+
+./pickle-dump.py "1.1" --output example.pickle
+runghc bin/pickle-read.hs example.pickle
+
+./pickle-dump.py "(1,)" --output example.pickle
+runghc bin/pickle-read.hs example.pickle
+
+./pickle-dump.py "(1, 2)" --output example.pickle
+runghc bin/pickle-read.hs example.pickle
+
+./pickle-dump.py "[1, 2]" --output example.pickle
+runghc bin/pickle-read.hs example.pickle
+
+./pickle-dump.py "[(1, 2)]" --output example.pickle
+runghc bin/pickle-read.hs example.pickle
+
+./pickle-dump.py "{'datapoints': [(1, 2)]}" --output example.pickle
+runghc bin/pickle-read.hs example.pickle
+
+./pickle-dump.py "{'datapoints': [(1, 2)], (2,): 'foo'}" --output example.pickle
+runghc bin/pickle-read.hs example.pickle
+
 rm -f example.pickle
